f663d7d034c660d54e1d806a620005d6fb7d47b2
luisrodelo <72672355+luisrodelo@users.noreply.github.com>
2023-10-19 15:30:46 -0500

    Add files via upload

---
+++ main/script.js
@@ -0,0 +1,115 @@
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
+
+
+
+
+
+
+
+if(document.querySelector('#container-slider')){
+    setInterval('funcionEjecutar("siguiente")',5000);
+ }
+ //------------------------------ LIST SLIDER -------------------------
+ if(document.querySelector('.listslider')){
+    let link = document.querySelectorAll(".listslider li a");
+    link.forEach(function(link) {
+       link.addEventListener('click', function(e){
+          e.anteriorentDefault();
+          let item = this.getAttribute('itlist');
+          let arrItem = item.split("_");
+          funcionEjecutar(arrItem[1]);
+          return false;
+       });
+     });
+ }
+ function funcionEjecutar(side){
+     let parentTarget = document.getElementById('slider');
+     let elements = parentTarget.getElementsByTagName('li');
+     let curElement, siguienteElement;
+     for(var i=0; i<elements.length;i++){
+         if(elements[i].style.opacity==1){
+             curElement = i;
+             break;
+         }
+     }
+     if(side == 'anterior' || side == 'siguiente'){
+         if(side=="anterior"){
+             siguienteElement = (curElement == 0)?elements.length -1:curElement -1;
+         }else{
+             siguienteElement = (curElement == elements.length -1)?0:curElement +1;
+         }
+     }else{
+         siguienteElement = side;
+         side = (curElement > siguienteElement)?'anterior':'siguiente';
+     }
+     
+     //PUNTOS INFERIORES
+     let elementSel = document.getElementsByClassName("listslider")[0].getElementsByTagName("a");
+     elementSel[curElement].classList.remove("item-select-slid");
+     elementSel[siguienteElement].classList.add("item-select-slid");
+     elements[curElement].style.opacity=0;
+     elements[curElement].style.zIndex =0;
+     elements[siguienteElement].style.opacity=1;
+     elements[siguienteElement].style.zIndex =1;
+ }
+
+
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+function App() {}
+
+window.onload = function (event) {
+    var app = new App();
+    window.app = app;
+};
+
+App.prototype.processingButton = function(event) {
+    const btn = event.currentTarget;
+    const slickList = event.currentTarget.parentNode;
+    const track = event.currentTarget.parentNode.querySelector('#track');
+    const slick = track.querySelectorAll('.slick');
+
+    const slickWidth = slick[0].offsetWidth;
+    
+    const trackWidth = track.offsetWidth;
+    const listWidth = slickList.offsetWidth;
+
+    track.style.left == ""  ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+
+    btn.dataset.button == "button-prev" ? prevAction(leftPosition,slickWidth,track) : nextAction(leftPosition,trackWidth,listWidth,slickWidth,track)
+}
+
+let prevAction = (leftPosition,slickWidth,track) => {
+    if(leftPosition > 0) {
+        console.log("entro 2")
+        track.style.left = `${-1 * (leftPosition - slickWidth)}`;
+    }
+}
+
+let nextAction = (leftPosition,trackWidth,listWidth,slickWidth,track) => {
+    if(leftPosition < (trackWidth - listWidth)) {
+        track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
+    }
+}
+
+
